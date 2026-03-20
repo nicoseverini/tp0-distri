0fb93874233327ce50e1debe96d9ee1433646556
nicoseverini <nicoseve823@gmail.com>
2026-03-20 15:09:49 -0300

    feat(ej2): agregar volumen para configuraciones de servidor y cliente en Docker Compose.
    - se elimino las variables de entorno de nivel de registro de la configuracion de Docker Compose.

--- generador.py
+++ generador.py
@@ -13,9 +13,10 @@ with open(output, "w") as f:
     f.write("    entrypoint: python3 /main.py\n")
     f.write("    environment:\n")
     f.write("      - PYTHONUNBUFFERED=1\n")
-    f.write("      - LOGGING_LEVEL=DEBUG\n")
     f.write("    networks:\n")
     f.write("      - testing_net\n")
+    f.write("    volumes:\n")
+    f.write("      - ./server/config.ini:/config.ini\n")
 
     f.write("    healthcheck:\n")
     f.write("      test: [\"CMD\", \"python3\", \"-c\", \"import socket; s=socket.socket(); s.settimeout(1); s.connect(('localhost',12345))\"]\n")
@@ -30,9 +31,10 @@ with open(output, "w") as f:
         f.write("    entrypoint: /client\n")
         f.write("    environment:\n")
         f.write(f"      - CLI_ID={i}\n")
-        f.write("      - CLI_LOG_LEVEL=DEBUG\n")
         f.write("    networks:\n")
         f.write("      - testing_net\n")
+        f.write("    volumes:\n")
+        f.write("      - ./client/config.yaml:/config.yaml\n")
 
         f.write("    depends_on:\n")
         f.write("      server:\n")
